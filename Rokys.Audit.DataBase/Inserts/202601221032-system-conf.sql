582a4dc39279293e765a641f9350366a89bba483
Ebert Escalante <ebertescalant@gmail.com>
2026-01-22 10:33:50 -0500

    feat: add initial system configuration insert script

---
+++ Rokys.Audit.DataBase/Inserts/202601221032-system-conf.sql
@@ -0,0 +1,2 @@
+INSERT INTO SystemConfiguration (SystemConfigurationId,ConfigKey,ConfigValue,DataType,Description,ReferenceType,ReferenceCode,IsActive,CreatedBy,CreationDate,UpdatedBy) VALUES
+	 (N'A14FDAA2-9A43-4051-959A-A63371094B20',N'EG001',N'96',N'INT',N'Valor maximo a aplicar un plan de accion',N'EnterpriseGrouping',N'EG001',1,N'Admin','2026-01-20 14:58:09.376',NULL);
